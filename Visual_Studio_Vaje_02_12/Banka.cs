bcbbade95f43ef03ab87ab18b40106e012c527b8
Tomas Petocelli <petocellitommy2@gmail.com>
2026-02-12 10:33:24 +0100

    Lekcija 12/02/2026

---
+++ Visual_Studio_Vaje_02_12/Banka.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visual_Studio_Vaje_02_12 {
+    internal class Banka {
+        double glavnica, obresnaMera, stLet;
+
+        public Banka(double glavnica, double obresnaMera, double stLet) {
+            this.glavnica = glavnica;
+            this.obresnaMera = obresnaMera;
+            this.stLet = stLet;
+        }//Konec konstruktor Banka
+
+        public double LetneObresti() {
+            return (glavnica * Math.Pow(1 + obresnaMera, stLet));
+        }//Konec LetneObresti
+
+        public double DnevneObresti() {
+            return (glavnica * Math.Pow(1 + obresnaMera / 365, stLet * 365));
+        }//Konec DnevneObresti
+
+        static void Main(string[] args) {
+            Console.Write("Vnesi glavnico: ");
+            double glavnica = double.Parse(Console.ReadLine());
+
+            Console.Write("Vnesi obrestno mero: ");
+            double obrestnaMera = double.Parse(Console.ReadLine()) / 100.0;
+
+            Console.Write("Vnesi število let: ");
+            double stLet = double.Parse(Console.ReadLine());
+
+            Banka b = new Banka(glavnica, obrestnaMera, stLet);
+
+            Console.WriteLine("Letne obresti:  " + b.LetneObresti());
+            Console.WriteLine("Dnevne obresti: " + b.DnevneObresti());
+
+        }//Konec Main
+    }//Konec class Banka
+}
---
+++ Visual_Studio_Vaje_02_12/Palcke.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visual_Studio_Vaje_02_12 {
+    internal class Palcke {
+        int stPalck;
+        int igralec;
+
+        public Palcke() {
+            this.stPalck = 7;
+            this.igralec = 1;
+        }//Konec Konstruktor Palcke
+
+        public int StPalck { get => stPalck; }
+
+        public bool VzemiPalcke() {
+            Console.Write("Vnesi stevilo palčk, ki jih želite vzeti: ");
+            int stVzetihPalck = int.Parse(Console.ReadLine());
+            if (stVzetihPalck >= 1 && stVzetihPalck <= 3) {
+                igralec = 3 - igralec;
+                stPalck -= stVzetihPalck;
+                return true;
+            } else {
+                return false;
+            }//Konec if-else
+        }//Konec metoda VzemiPalcke
+
+        public bool KonecIgre() {
+            if (stPalck <= 0) {
+                return true;
+            } else {
+                return false;
+            }//Konec if-else
+        }//Konec metode KonecIgre
+
+        public void DobiZmagovalca() {
+            if (stPalck < 1) {
+                Console.WriteLine("\nZmagal je igralec: " +igralec + ".");
+            }//Konec if
+        }//Konec metode DobiZmagovalca
+
+        public void Izpis() {
+            Console.Write("Na mizi je še " +stPalck +" palčk");
+            Console.WriteLine("\tNa vrsti je igralec: " + igralec);
+        }
+
+        public static void Main(string[] args) {
+            Palcke p = new Palcke();
+
+            while (p.KonecIgre() != true) { 
+                p.Izpis();
+                p.VzemiPalcke();
+                p.DobiZmagovalca();
+            }//Konec while
+        }//Konec Main
+    }//Konec class Palcke
+}
